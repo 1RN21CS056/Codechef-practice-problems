eba09f8de375895836adbb23ef977f5b7adcfeb4
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-02-13 21:50:06 +0530

    Add files via upload

---
+++ MY1STCONTEST.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b;
+	cin>>n>>a>>b;
+	int ans=(n-a);
+	cout<<ans<<endl;
+	int temp=(ans-b);
+	cout<<temp;
+	return 0;
+}
