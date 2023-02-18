fc21c53cdf6d30ef9b2205ad58f9b1fafd1eaef8
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-02-18 18:12:11 +0530

    Add files via upload

---
+++ SUBSCRIBE.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x<=30)
+	    {
+	        cout<<"no"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	}
+	return 0;
+}
