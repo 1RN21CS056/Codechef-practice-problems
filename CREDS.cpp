46128554914c703d850c15a268943c4344c2e86b
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-02-18 17:50:30 +0530

    Add files via upload

---
+++ CREDS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    int sum=((4*x)+(2*y)+(0*z));
+	    cout<<sum<<endl;
+	}
+	return 0;
+}
