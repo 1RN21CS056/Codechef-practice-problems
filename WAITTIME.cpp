cbc67e277c37977239613f20152b0b071b75eb27
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-02-19 09:32:15 +0530

    Add files via upload

---
+++ WAITTIME.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   int k,x;
+	   cin>>k>>x;
+	   int res=((7*k)-x);
+	   cout<<res<<endl;
+	   
+	}
+	return 0;
+}
