9d203dd893ca9f7a0ebf05b088231bfb14f74ff0
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-02-21 21:40:37 +0530

    Add files via upload

---
+++ MAXDIFFMIN.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+//#include<bits/stdc++.h>
+//#define ll long long
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<c-a<<endl;
+    }
+}
---
+++ PODIUM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    int res=(a+b);
+	    cout<<res<<endl;
+	}
+	return 0;
+}
