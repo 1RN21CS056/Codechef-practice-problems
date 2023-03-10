c11d532db7cce185932d3514e88910c4684aee5f
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-03-10 22:46:21 +0530

    Add files via upload

---
+++ CWIREFRAME.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	        int n,m,x;
+	        cin>>n>>m>>x;
+	        int temp=(2*n + 2*m);
+	        cout<<(temp*x)<<endl;
+	        
+	}
+	return 0;
+}
