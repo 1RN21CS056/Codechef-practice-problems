79897c422290c25e45f1ea8127a5295b4d66d9c8
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-03-15 20:40:56 +0530

    Add files via upload

---
+++ DISCNT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int testcase;
+	cin >>  testcase;
+	while(testcase--){
+	    int x;
+	    cin >> x;
+	    int cal;
+	    cal = 100 -x;
+	    cout << cal << endl;
+	}
+	return 0;
+}
