327ab5c2a45f7777d669e639115df4a8a1de9d83
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-03-26 09:26:02 +0530

    Add files via upload

---
+++ AIANALYSE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes her
+	    int x;
+	    cin>>x;
+	    if(x<=1000)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	return 0;
+}
