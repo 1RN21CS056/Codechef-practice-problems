c72a84ef5989fca0469922ec4f40360c216055a0
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-04-15 10:00:35 +0530

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    cout<<(x*1000)/100<<endl;
+	}
+	return 0;
+}
