0c204ae39a0cdb848f3d4e55c6147cf4675007f7
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-04-17 17:45:00 +0530

    Add files via upload

---
+++ OFFICE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int a,b;
+        cin>>a>>b;
+        cout<<(a*4+b)<<endl;
+    }
+	// your code goes here
+	return 0;
+}
