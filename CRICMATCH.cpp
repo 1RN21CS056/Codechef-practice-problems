ad22e42c99ddff93a9b1a39a71ff16f01a3d32f1
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-04-21 19:25:04 +0530

    Add files via upload

---
+++ CRICMATCH.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a<=36*b)cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+	return 0;
+}
