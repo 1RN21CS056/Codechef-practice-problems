8e49339acef19d62bfe1ca2c2237a923b7e164a2
Eshwar k <117366022+1RN21CS056@users.noreply.github.com>
2023-05-16 12:23:21 +0530

    Add files via upload

---
+++ WINNERR.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int a,b,x,y;
+        cin>>a>>b>>x>>y;
+        int c=max(x,y);
+	    int d=max(a,b);
+	    if(c==d){
+	       cout<<"TIE"<<endl;
+	    }else if(c<d){
+	        cout<<"Q"<<endl;
+	    }else{
+	        cout<<"P"<<endl;
+	    }
+        
+        
+    }
+	// your code goes here
+	return 0;
+}
